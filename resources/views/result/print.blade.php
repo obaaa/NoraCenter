9e01a222d76b6ec93696b01a8e4e22966d02082d
obaaa8 <obaaa8@gmail.com>
2018-08-26 01:53:56 +0200

    عرض الشهادة

---
+++ resources/views/result/print.blade.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Receipt</title>
+
+  <!-- Normalize or reset CSS with your favorite library -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <!-- Load paper.css for happy printing -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.4.1/paper.css">
+
+  <!-- Set page size here: A5, A4 or A3 -->
+  <!-- Set also "landscape" if you need -->
+  <style>@page { size: A4 landscape }</style>
+
+  <!-- Custom styles for this document -->
+
+<link href="https://fonts.googleapis.com/css?family=Diplomata+SC" rel="stylesheet">
+
+<link href="https://fonts.googleapis.com/css?family=Libre+Barcode+39+Extended" rel="stylesheet">
+
+  <link href='https://fonts.googleapis.com/css?family=Tangerine:700' rel='stylesheet' type='text/css'>
+  <style>
+    body   { font-family: 'Markazi Text', serif; text-align: center; margin: 5mm}
+    h1     { font-family: 'Tangerine', cursive; font-size: 40pt; line-height: 18mm}
+    h2, h3 { font-family: 'Tangerine', cursive; font-size: 24pt; line-height: 7mm }
+    h4     { font-size: 32pt; line-height: 14mm }
+    h2 + p { font-size: 18pt; line-height: 7mm }
+    h3 + p { font-size: 14pt; line-height: 7mm }
+    li     { font-size: 11pt; line-height: 5mm }
+    h1      { margin: 0 }
+    h1 + ul { margin: 2mm 0 5mm }
+    h2, h3  { margin: 0 3mm 3mm 0; float: left }
+    h2 + p,
+    h3 + p  { margin: 0 0 3mm 50mm }
+    h4      { margin: 2mm 0 0 50mm; border-bottom: 2px solid black }
+    h4 + ul { margin: 5mm 0 0 50mm }
+    article { border: 4px double black; padding: 5mm 10mm; border-radius: 3mm }
+    /* section { border: 5px double black; } */
+  </style>
+</head>
+<!-- Set "A5", "A4" or "A3" for class name -->
+<!-- Set also "landscape" if you need -->
+<body class="A4 landscape">
+  
+  <!-- Each sheet element should have the class "sheet" -->
+  <!-- "padding-**mm" is optional: you can set 10, 15, 20 or 25 -->
+  <section class="sheet padding-10mm container">
+        <div class="row align-items-start" style="text-align: center; float:center;">
+          <div class="col">
+            <img src="{{ asset($trainee_photo) }}" class="rounded-top" alt="" height="160px" width="150px">
+            <p>
+              رقم المتدرب<br>
+              @php
+                $trainee_id = str_pad($trainee_id,7,'0',STR_PAD_LEFT);
+              @endphp
+              {{ $trainee_id }}
+              {{-- <p style="border: 4px double black;"><span style="font-size: 20pt; font-family: 'Libre Barcode 39 Extended', cursive;">{{ $id }}</span></p> --}}
+            </p>
+          </div>
+          <div class="col-7">
+            <p style="font-size: 8pt;font-family: 'Amiri', serif; text-align: center;">بسم الله الرحمن الرحيم</p>
+            <p style="font-size: 20pt;font-family: 'Markazi Text', serif; text-align: center;">جمهورية السودان</p>
+            <p style="font-size: 22pt; line-height: 8mm;font-family: 'Markazi Text', serif; text-align: center;"><b>مركز الزعيم للتدريب والتنمية البشرية</b></p>
+            <p style="font-size: 18pt; line-height: 10mm;font-family: 'Titillium Web', sans-serif; text-align: center;"><b>ALZAIEM CENTER <br> FOR TRAINING & PERSONAL DEVELOPMENT</b></p>
+            <p style="font-size: 20pt; line-height: 13mm;font-family: 'Diplomata SC', cursive; text-align: center;">CERTIFICATE</p>
+          </div>
+          <div class="col" style="text-align: center; float:center;">
+            <img title='{!!(CRUDBooster::getSetting('appname') == 'CRUDBooster')?"<b>CRUD</b>Booster":CRUDBooster::getSetting('appname')!!}' src='{{ CRUDBooster::getSetting("logo")?asset(CRUDBooster::getSetting('logo')):asset('vendor/crudbooster/assets/logo_crudbooster.png') }}' style='max-width: 200px;max-height:150px'/>
+            <p>رقم التسجيل<br>1625</p>
+          </div>
+        </div>
+        <hr>
+        <div class="row text-align-left" style="font-size: 18pt; line-height: 10mm;font-family: 'Titillium Web', sans-serif; text-align: left;">
+          <div class="col">
+            <div>
+                This is certify that: <code><b>{{ $trainee_name }}</b></code>
+                Has successfully completed the following course:
+            </div>
+            <div style="text-align: center;">
+                <br>
+                <h1><code>{{ $course_name }}</code></h1>
+            </div>
+            <div>
+              During the period from: <code>{{$group_start}}</code> to <code>{{ $group_end }}</code>
+              and has been awarded this certificate with the degree: <code>{{ $degree }}%</code>
+            </div>
+          </div>
+        </div>
+        <br><br>
+        <div class="row justify-content-center" style="text-align: center; float:center;">
+          <div class="col">
+            <b>Principal</b>
+          </div>
+          <div class="col-5">
+              
+          </div>
+          <div class="col">
+            <b>Supervisor</b>
+          </div>
+        </div>
+    </h5> --}} <br><br><br>
+    <div class="row align-items-end">
+    
+    <div class="col">
+    <code><p>للتأكد من صحة الشهادة <br></code>
+      <code>{{ config('app.url').'/verify/'.$verify }}</code>
+    </p>
+      
+    </div>
+  </div>
+  </section>
+
+</body>
+
+</html>
