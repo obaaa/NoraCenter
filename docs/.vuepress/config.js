0c9e0e6dd05d11296db98c16567f928dcacbce6d
obaaa8 <obaaa8@gmail.com>
2018-08-30 23:01:54 +0200

    إضافة مسارات جديدة

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,10 +8,38 @@ module.exports = {
         ],
         sidebar: [
             {
-                title: 'Content',
-                collapsable: false,
+                title: 'documentation',
+                collapsable: true,
                 children: [
                 '/content/'
+                ]
+            },
+            {
+                title: 'Setting',
+                collapsable: true,
+                children: [
+                '/setting/',
+                '/setting/app.md',
+                '/setting/branche.md',
+                '/setting/classroom.md',
+                '/setting/classroom_lectures.md',
+                '/setting/specialties.md',
+                '/setting/courses.md',
+                '/setting/manager.md'
+                ]
+            },
+            {
+                title: 'Employees',
+                collapsable: true,
+                children: [
+                '/employees/',
+                '/employees/receptionist.md',
+                '/employees/accountant.md',
+                '/employees/trainer.md',
+                // '/employees/classroom_lectures.md',
+                // '/employees/specialties.md',
+                // '/employees/courses.md',
+                // '/employees/manager.md'
                 ]
             }
             ]
