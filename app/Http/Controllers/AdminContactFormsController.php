451934d7807846deb2a7df782b4e6f157b4556fc
obaaa8 <obaaa8@gmail.com>
2018-09-02 23:52:49 +0200

    متحكم الرسائل

---
+++ app/Http/Controllers/AdminContactFormsController.php
@@ -0,0 +1,375 @@
+<?php namespace App\Http\Controllers;
+
+	use Session;
+	use Request;
+	use DB;
+	use CRUDBooster;
+
+	class AdminContactFormsController extends \crocodicstudio\crudbooster\controllers\CBController {
+
+	    public function cbInit() {
+
+			# START CONFIGURATION DO NOT REMOVE THIS LINE
+			$this->title_field = "name";
+			$this->limit = "20";
+			$this->orderby = "id,desc";
+			$this->global_privilege = true;
+			$this->button_table_action = true;
+			$this->button_bulk_action = true;
+			$this->button_action_style = "button_icon";
+			$this->button_add = false;
+			$this->button_edit = false;
+			$this->button_delete = false;
+			$this->button_detail = false;
+			$this->button_show = false;
+			$this->button_filter = true;
+			$this->button_import = false;
+			$this->button_export = false;
+			$this->table = "contact_forms";
+			# END CONFIGURATION DO NOT REMOVE THIS LINE
+
+			# START COLUMNS DO NOT REMOVE THIS LINE
+			$this->col = [];
+			$this->col[] = ["label"=>"Name","name"=>"name"];
+			$this->col[] = ["label"=>"Email","name"=>"email"];
+			// $this->col[] = ["label"=>"Subject","name"=>"subject"];
+			// $this->col[] = ["label"=>"Movement","name"=>"movement"];
+			$this->col[] = ["label"=>"Read","name"=>"is_read"];
+			# END COLUMNS DO NOT REMOVE THIS LINE
+
+			# START FORM DO NOT REMOVE THIS LINE
+			$this->form = [];
+			$this->form[] = ['label'=>'Subject','name'=>'subject','type'=>'text','validation'=>'required|min:1|max:255','width'=>'col-sm-10'];
+			$this->form[] = ['label'=>'Contact Message','name'=>'contact_message','type'=>'textarea','validation'=>'required|string|min:5|max:5000','width'=>'col-sm-10'];
+			# END FORM DO NOT REMOVE THIS LINE
+
+			# OLD START FORM
+			//$this->form = [];
+			//$this->form[] = ["label"=>"Name","name"=>"name","type"=>"text","required"=>TRUE,"validation"=>"required|string|min:3|max:70","placeholder"=>"فضلا ادخل احرف فقط"];
+			//$this->form[] = ["label"=>"Email","name"=>"email","type"=>"email","required"=>TRUE,"validation"=>"required|min:1|max:255|email|unique:contact_forms","placeholder"=>"صيغة البريد الإلكتروني غير صحيحة"];
+			//$this->form[] = ["label"=>"Subject","name"=>"subject","type"=>"text","required"=>TRUE,"validation"=>"required|min:1|max:255"];
+			//$this->form[] = ["label"=>"Contact Message","name"=>"contact_message","type"=>"textarea","required"=>TRUE,"validation"=>"required|string|min:5|max:5000"];
+			# OLD END FORM
+
+			/*
+	        | ----------------------------------------------------------------------
+	        | Sub Module
+	        | ----------------------------------------------------------------------
+			| @label          = Label of action
+			| @path           = Path of sub module
+			| @foreign_key 	  = foreign key of sub table/module
+			| @button_color   = Bootstrap Class (primary,success,warning,danger)
+			| @button_icon    = Font Awesome Class
+			| @parent_columns = Sparate with comma, e.g : name,created_at
+	        |
+	        */
+	        $this->sub_module = array();
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Add More Action Button / Menu
+	        | ----------------------------------------------------------------------
+	        | @label       = Label of action
+	        | @url         = Target URL, you can use field alias. e.g : [id], [name], [title], etc
+	        | @icon        = Font awesome class icon. e.g : fa fa-bars
+	        | @color 	   = Default is primary. (primary, warning, succecss, info)
+	        | @showIf 	   = If condition when action show. Use field alias. e.g : [id] == 1
+	        |
+	        */
+	        $this->addaction = array();
+          $this->addaction[] = ['labelurl'=>CRUDBooster::mainpath('detail/[id]'),'icon'=>'fa fa-arrow-right','color'=>'primary'];
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Add More Button Selected
+	        | ----------------------------------------------------------------------
+	        | @label       = Label of action
+	        | @icon 	   = Icon from fontawesome
+	        | @name 	   = Name of button
+	        | Then about the action, you should code at actionButtonSelected method
+	        |
+	        */
+	        $this->button_selected = array();
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Add alert message to this module at overheader
+	        | ----------------------------------------------------------------------
+	        | @message = Text of message
+	        | @type    = warning,success,danger,info
+	        |
+	        */
+	        $this->alert        = array();
+
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Add more button to header button
+	        | ----------------------------------------------------------------------
+	        | @label = Name of button
+	        | @url   = URL Target
+	        | @icon  = Icon from Awesome.
+	        |
+	        */
+	        $this->index_button = array();
+
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Customize Table Row Color
+	        | ----------------------------------------------------------------------
+	        | @condition = If condition. You may use field alias. E.g : [id] == 1
+	        | @color = Default is none. You can use bootstrap success,info,warning,danger,primary.
+	        |
+	        */
+	        $this->table_row_color = array();
+          $this->table_row_color[] = ['condition'=>"[is_read] == null","color"=>"danger"];
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | You may use this bellow array to add statistic at dashboard
+	        | ----------------------------------------------------------------------
+	        | @label, @count, @icon, @color
+	        |
+	        */
+	        $this->index_statistic = array();
+          if (in_array(CRUDBooster::myPrivilegeId(), [1,2], TRUE)){
+          $this->index_statistic[] = ['label'=>'Unread','count'=>DB::table('contact_forms')->where('is_read',null)->count(),'icon'=>'fa fa-square-o','color'=>'danger'];
+          $this->index_statistic[] = ['label'=>'Read','count'=>DB::table('contact_forms')->where('is_read',1)->count(),'icon'=>'fa fa-check-square-o','color'=>'success'];
+          }
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Add javascript at body
+	        | ----------------------------------------------------------------------
+	        | javascript code in the variable
+	        | $this->script_js = "function() { ... }";
+	        |
+	        */
+	        $this->script_js = NULL;
+
+
+            /*
+	        | ----------------------------------------------------------------------
+	        | Include HTML Code before index table
+	        | ----------------------------------------------------------------------
+	        | html code to display it before index table
+	        | $this->pre_index_html = "<p>test</p>";
+	        |
+	        */
+	        $this->pre_index_html = null;
+
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Include HTML Code after index table
+	        | ----------------------------------------------------------------------
+	        | html code to display it after index table
+	        | $this->post_index_html = "<p>test</p>";
+	        |
+	        */
+	        $this->post_index_html = null;
+
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Include Javascript File
+	        | ----------------------------------------------------------------------
+	        | URL of your javascript each array
+	        | $this->load_js[] = asset("myfile.js");
+	        |
+	        */
+	        $this->load_js = array();
+
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Add css style at body
+	        | ----------------------------------------------------------------------
+	        | css code in the variable
+	        | $this->style_css = ".style{....}";
+	        |
+	        */
+	        $this->style_css = NULL;
+
+
+
+	        /*
+	        | ----------------------------------------------------------------------
+	        | Include css File
+	        | ----------------------------------------------------------------------
+	        | URL of your css each array
+	        | $this->load_css[] = asset("myfile.css");
+	        |
+	        */
+	        $this->load_css = array();
+
+
+	    }
+
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for button selected
+	    | ----------------------------------------------------------------------
+	    | @id_selected = the id selected
+	    | @button_name = the name of button
+	    |
+	    */
+	    public function actionButtonSelected($id_selected,$button_name) {
+	        //Your code here
+
+	    }
+
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for manipulate query of index result
+	    | ----------------------------------------------------------------------
+	    | @query = current sql query
+	    |
+	    */
+	    public function hook_query_index(&$query) {
+
+        // $contact =DB::table('contact_forms')->get();
+        // $contact = collect($contact);
+        // $contact = $contact->groupBy('email');
+        // foreach ($contact as $value) {
+        //   $last_created_at = $value->max('created_at');
+        //   $last_contact[] = $value->where('created_at',$last_created_at)->first();
+        //
+        // }
+        // foreach ($last_contact as $value) {
+        //   DB::table('contact_forms')->where('id',$value->id)->update([
+        //     'is_last' => 1
+        //   ]);
+        // }
+
+        // DB::table('contact_forms')->update([
+        //     'is_read' => 1
+        //   ]);
+
+	        //Your code here
+          if (!in_array(CRUDBooster::myPrivilegeId(), [1,2], TRUE)){
+           $query->where('contact_forms.email', DB::table('cms_users')->where('id',CRUDBooster::myId())->value('email'));
+         }
+         $query->where('contact_forms.is_last',1)->orderBy('contact_forms.is_read');
+	    }
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for manipulate row of index table html
+	    | ----------------------------------------------------------------------
+	    |
+	    */
+	    public function hook_row_index($column_index,&$column_value) {
+	    	//Your code here
+        // if ($column_index == 3) {
+        //   if (in_array(CRUDBooster::myPrivilegeId(), [1,2], TRUE)){
+        //     if ($column_value == 'out') {
+        //       $column_value = '<i class="fa fa-arrow-down" aria-hidden="true"></i>';
+        //     }else {
+        //       $column_value = '<i class="fa fa-arrow-up" aria-hidden="true"></i>';
+        //     }
+        //   }else {
+        //     if ($column_value == 'in') {
+        //       $column_value = '<i class="fa fa-arrow-down" aria-hidden="true"></i>';
+        //     }else {
+        //       $column_value = '<i class="fa fa-arrow-up" aria-hidden="true"></i>';
+        //     }
+        //   }
+        // }
+	    	//Your code here
+        if ($column_index == 3)
+          if ($column_value == 1)
+            $column_value = '<i class="fa fa-check-square-o" aria-hidden="true"></i>';
+      }
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for manipulate data input before add data is execute
+	    | ----------------------------------------------------------------------
+	    | @arr
+	    |
+	    */
+	    public function hook_before_add(&$postdata) {
+	        //Your code here
+          if (in_array(CRUDBooster::myPrivilegeId(), [1,2], TRUE))
+            $postdata['movement'] = 'in';
+	    }
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for execute command after add public static function called
+	    | ----------------------------------------------------------------------
+	    | @id = last insert id
+	    |
+	    */
+	    public function hook_after_add($id) {
+	        //Your code here
+
+	    }
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for manipulate data input before update data is execute
+	    | ----------------------------------------------------------------------
+	    | @postdata = input post data
+	    | @id       = current id
+	    |
+	    */
+	    public function hook_before_edit(&$postdata,$id) {
+	        //Your code here
+
+	    }
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for execute command after edit public static function called
+	    | ----------------------------------------------------------------------
+	    | @id       = current id
+	    |
+	    */
+	    public function hook_after_edit($id) {
+	        //Your code here
+
+	    }
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for execute command before delete public static function called
+	    | ----------------------------------------------------------------------
+	    | @id       = current id
+	    |
+	    */
+	    public function hook_before_delete($id) {
+	        //Your code here
+
+	    }
+
+	    /*
+	    | ----------------------------------------------------------------------
+	    | Hook for execute command after delete public static function called
+	    | ----------------------------------------------------------------------
+	    | @id       = current id
+	    |
+	    */
+	    public function hook_after_delete($id) {
+	        //Your code here
+
+	    }
+
+
+
+	    //By the way, you can still create your own method in here... :)
+
+
+	}
