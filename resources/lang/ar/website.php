e4304385b256dfc2a056e92683ff8a87eb9ed168
obaaa8 <obaaa8@gmail.com>
2018-09-15 11:42:19 +0200

    ملف اللغة العربية الخاص بموقع الويب

---
+++ resources/lang/ar/website.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'about' => 'عنا',
+    'home' => 'الرئيسية',
+    'to_connect' => 'للتواصل',
+    'events' => 'اﻷحداث',
+    'trainers' => 'المدربين',
+    'specialties' => 'التخصصات',
+    'system' => 'الدخول للنظام',
+    'app' => 'App',
+    'user_name' => 'اﻹسم',
+    'email' => 'البريد اﻹلكتروني',
+    'message' => 'الرسالة',
+    'submit' => 'إرسال',
+    'branches' => 'الفروع',
+    'courses' => 'الكورسات',
+    'trainees' => 'المتدربين',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
