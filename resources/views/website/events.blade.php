446cc5535ec9614f6c931e780a63f17bf376a58b
obaaa8 <obaaa8@gmail.com>
2018-09-15 11:43:31 +0200

    صفخة الاحداث لموقع الويب

---
+++ resources/views/website/events.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.web')
+
+@section('title', 'اﻷحداث')
+
+
+@section('content')
+  <section class="hero is-light is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          events
+        </h1>
+      </div>
+    </div>
+  </section>
+  <p>events</p>
+@endsection
