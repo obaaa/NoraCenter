e3fd4b9a5477abd6777d387d2e54fecec7d4115f
obaaa8 <obaaa8@gmail.com>
2018-09-16 09:51:55 +0200

    فصل العرض

---
+++ resources/views/layouts/asset.blade.php
@@ -0,0 +1,9 @@
+  <meta charset="UTF-8">
+  <title>{{ CRUDBooster::getSetting('appname') }} - @yield('title')</title>
+  <meta name="csrf-token" content="{{ csrf_token() }}"/>
+  <meta name='generator' content='NoraCenter'/>
+  <link rel="shortcut icon" href="{{ CRUDBooster::getSetting('favicon')?asset(CRUDBooster::getSetting('favicon')):asset('vendor/crudbooster/assets/logo_crudbooster.png') }}">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset("website/css/bulma-rtl.css") }}">
+  <link href="https://fonts.googleapis.com/css?family=Cairo" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma-extensions@2.2.2/dist/js/bulma-extensions.min.js">
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,23 @@
+<footer class="footer">
+  <section class="hero is-smale">
+    <div class="hero-foot">
+      <div class="container has-text-centered">
+        {{-- <p><br>
+            <strong>{{ CRUDBooster::getSetting('appname') }}</strong> by <a href="http://obaaa.sd">Obaaa</a>
+        </p> --}}
+        <a href="http://obaaa.sd" target="_blank" style="color:#3e4347">
+          <h4>
+            {{ CRUDBooster::getSetting('appname') }}
+            ≺≻ with 🖤 by Obaaa
+          </h4>
+        </a>
+      </div>
+    </div>
+  </section>
+</footer>
+{{--
+<footer>
+  <center>
+
+  </center>
+</footer> --}}
