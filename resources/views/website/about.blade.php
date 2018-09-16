185601a3ecb21df38a34cc796abf59e1d3b14556
obaaa8 <obaaa8@gmail.com>
2018-09-16 09:49:55 +0200

    تحديث

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -9,7 +9,7 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title">
-          {{ trans("website.about") }}
+          {{-- {{ trans("website.about") }} --}}
         </h1>
       </div>
     </div>
