a263c39fb62749b973e612e00830a959229d96e0
obaaa8 <obaaa8@gmail.com>
2018-09-21 00:23:11 +0200

    update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,10 @@
-<footer class="footer">
+{{-- <footer class="footer">
   <section class="hero is-smale">
     <div class="hero-foot">
       <div class="container has-text-centered">
         {{-- <p><br>
             <strong>{{ CRUDBooster::getSetting('appname') }}</strong> by <a href="http://obaaa.sd">Obaaa</a>
-        </p> --}}
+        </p> -- }}
         <a href="http://obaaa.sd" target="_blank" style="color:#3e4347">
           <h4>
             {{ CRUDBooster::getSetting('appname') }}
@@ -14,6 +14,18 @@
       </div>
     </div>
   </section>
+</footer> --}}
+<footer class="footer">
+    <div class="container">
+        <div class="content has-text-centered">
+          <p class="is-6">
+            <a href="http://obaaa.sd" target="_blank" style="color:#3e4347">
+              {{ CRUDBooster::getSetting('appname') }}
+               ≺≻ with <i class="fa fa-heart" aria-hidden="true"></i>  by Obaaa
+             </a>
+          </p>
+        </div>
+    </div>
 </footer>
 {{--
 <footer>
