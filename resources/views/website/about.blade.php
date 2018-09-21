96cd67ec206b420cefb93a63fdced20837ac44e5
obaaa8 <obaaa8@gmail.com>
2018-09-21 14:19:50 +0200

    اضافة الفروع

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -9,7 +9,7 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title">
-          {{-- {{ trans("website.about") }} --}}
+          {{ trans("website.about") }}
         </h1>
       </div>
     </div>
@@ -25,4 +25,24 @@
     </div>
   </section>
 
+  <div class="container">
+    <hr><br>
+  </div>
+
+    <div class="container">
+      <h2 class="title has-text-rigth is-5">{{ trans("website.branches") }}</h2>
+      @foreach ($branches as $branche)
+        <div class="notification is-primary">
+          <div class="address">
+            <i class="fa fa-map-marker pull-right"></i><span>&nbsp;&nbsp;{{ $branche->location }}</span>
+          </div>
+          <div class="phone">
+            <i class="fa fa-phone pull-right"></i><span>&nbsp;&nbsp;{{ $branche->phone }}</span>
+          </div>
+          <div class="email">
+            <i class="fa fa-envelope pull-right"></i><span>&nbsp;&nbsp;{{ $branche->email }}</span>
+          </div><br>
+        </div>
+      @endforeach
+    </div>
 @endsection
